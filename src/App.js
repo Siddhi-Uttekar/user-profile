a9397f6979dbe2f50f1e923b86e687e363499b27
Siddhi-Uttekar <uttekarsiddhi145@gmail.com>
2025-02-09 16:41:35 +0530

    build the app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ProfileCard from './ProfileCard';
+
+function App() {
+  return (
+    <div className= " bg-black  min-h-screen flex items-center justify-center">
+<ProfileCard/>
+  </div>
+  );
+}
+
+export default App;
